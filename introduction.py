6ba567885fd2a94aaaea8e5187488591145214cf
Shoyeb45 <shoyebff45@gmail.com>
2024-01-31 16:22:38 +0530

    series

--- introduction.py
+++ introduction.py
@@ -14,12 +14,15 @@ age = dataFrame["Age"]
 gender = dataFrame["Name"]
 
 print(type(age), type(gender))
-
 print(age,"\n",gender)
-
 marks = [90, 98, 100, 67, 50]
+
+# series
 
 mark = pd.Series(marks)
 print(mark)
 print(type(mark))
+
+height = pd.Series([123.541, 89.433, 98.327, 12.455, 98.234])
+print(height)
 
