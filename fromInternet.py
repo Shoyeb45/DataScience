25356145ef833f0a386eeae5bf8709d6e2d73458
Shoyeb45 <shoyebff45@gmail.com>
2024-02-08 21:24:04 +0530

    Reading and writing files

---
+++ fromInternet.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+df = pd.read_csv("https://raw.githubusercontent.com/datasciencedojo/datasets/master/titanic.csv")
+
+print(df)
+
+# reading file from html
+
+dfHTML = pd.read_html("")
---
+++ readWrite.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# df = pd.read_csv("cardekhoDataset.csv")
+# dfJSON = pd.read_json("unece.json")
+
+# print(df,"\n", dfJSON)
+
+# dfExcel = pd.read_excel("titanic.xlsx")
+
+# print(dfExcel.info())
+
+
+
