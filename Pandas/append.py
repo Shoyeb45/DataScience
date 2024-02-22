143e2d2544179aef378c8c2104d6fed134c9cb53
Shoyeb45 <shoyebff45@gmail.com>
2024-02-22 18:49:37 +0530

    aggregate, gropuby and pivot tables

--- Pandas/append.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.DataFrame({'Roll No.':[1,2,3,4,5], 'Maths':[45,78,45,90,66], 'Physics':[33,67,12,90,44]})
-df2 = pd.DataFrame({'Roll No.':[6,7,8,9,10], 'Maths':[78,73,45,90,69], 'Physics':[23,67,88,0,98]})
-# df1.append
-dfAppended2= df2.add(df1)
-# print(f"df1 to df2\n{dfAppended1}")
-print(f"df2 to df1\n{dfAppended2}")
