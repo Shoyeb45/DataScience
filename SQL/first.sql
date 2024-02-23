b5726989637d3e91ba8e333e6e2eb929fa52fae5
Shoyeb45 <shoyebff45@gmail.com>
2024-02-23 10:54:30 +0530

    sql - queries

--- SQL/first.sql
+++ SQL/first.sql
@@ -1,1 +1,21 @@
+-- creating data base 
+
 create database pw;
+
+-- using data base first to perform query  
+use pw;
+
+-- displaying complete table,  
+select * from marks;
+
+-- displaying columns from table 
+select Physics from marks; 
+
+-- Selecting distinct values from columns 
+select DISTINCT computer From marks;
+
+-- multiple columns 
+select Physics, Maths from marks;
+
+-- alias of column 
+select Physics as phy from marks;
