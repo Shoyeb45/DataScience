55e636970e40de22a82b7f5be956d6e7cb411192
Shoyeb45 <shoyebff45@gmail.com>
2024-03-01 11:16:11 +0530

    select, from and where

--- SQL/first.sql
+++ SQL/first.sql
@@ -8,6 +8,9 @@ use pw;
 -- displaying complete table,  
 select * from marks;
 
+
+
+
 -- displaying columns from table 
 select Physics from marks; 
 
@@ -18,4 +21,32 @@ select DISTINCT computer From marks;
 select Physics, Maths from marks;
 
 -- alias of column 
-select Physics as phy from marks;
+select Physics as phy, Maths from marks;
+
+--  Count Keyword
+select count(Physics) as phy from marks;
+select count(*) as TotalRecords from marks;
+
+-- count and distinct
+select count(distinct Maths) from marks; 
+
+-- Limit : It will show given number of rows 
+select 
+	* 	
+from marks
+LIMIT 3;
+
+-- Where Keyword
+select * from data; 
+select 
+	Duration , Pulse
+from data
+where Duration > 120 ;
+
+-- <> :- not equal
+select 
+	Duration
+from data
+where	Duration <> 100;
+
+
