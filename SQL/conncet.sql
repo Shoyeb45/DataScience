809d39f60f4840172dd5696d4a7076eebcb47405
Shoyeb45 <shoyebff45@gmail.com>
2024-03-05 13:43:50 +0530

    connecting with python

---
+++ SQL/conncet.sql
@@ -0,0 +1,15 @@
+use connect;
+
+create table Info (
+Id int,
+name varchar(100)
+);
+
+insert into Info values 
+(1, "Durgesh"), 
+(2, "Shoyeb"),
+(3, "Ravi");
+
+show tables;
+
+select * from info;
