9aaedf7f107ffc0e7c28a9377d12b49f50137300
Shoyeb45 <shoyebff45@gmail.com>
2024-06-01 07:09:31 +0530

    Vectors

---
+++ R_Programming/R_Programming/Vectors/rep.R
@@ -0,0 +1,17 @@
+# replicate 1 element multiple time
+x <- rep(2, times = 324)
+x
+
+# Replicate each element multiple times
+cities <- c("Banglalore", "Jaipur", "Ahemdabad")
+y <- rep(cities, each = 3)
+y
+
+
+# Replicate the entire vector multiple times
+z <- rep(cities, times= 2)
+z
+
+# Replicate each element multiple times with each element having a different number of replications
+h <- rep(cities, times = c(1, 4, 3))
+h
