78600e4be9f681687cfacd44214ac9d4b8364c52
Shoyeb45 <shoyebff45@gmail.com>
2024-06-01 06:30:44 +0530

    Atomic vectors

---
+++ R_Programming/R_Programming/Vectors/atomicVectors.R
@@ -0,0 +1,13 @@
+# So vectors are the most basic data structure in R, and they must contain elements of the same class.
+# Diff types of vectors
+# Logical vector - c() is used to create vector
+a<-c(T, F, T)
+
+# Integer vectors
+i<-c(1,9, 10, 123)
+
+# Double vectors
+d <- c(1.232, 1.234, 3.14)
+
+# Character vector
+ch <- c("g", "h", "a")
