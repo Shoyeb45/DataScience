11ab9abee180e5773f87d135a4703043ce1e6476
Shoyeb45 <shoyebff45@gmail.com>
2024-06-22 23:03:22 +0530

    if-else statemenet

---
+++ R_Programming/R_Programming/Conditional/ifelse.R
@@ -0,0 +1,15 @@
+# if statement
+num = 10
+if(num > 0) {
+  print("num is positive")
+} else {
+  print("num is negative")
+}
+num = 0
+if(num > 0) {
+  print("num is positive")
+} else if(num == 0) {
+  print("num is zero")
+} else {
+  print("num is negative")
+}
