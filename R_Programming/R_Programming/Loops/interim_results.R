f8132928fb950fa466a9e625eb547280ae3eb7f3
Shoyeb45 <shoyebff45@gmail.com>
2024-06-28 10:57:32 +0530

    Interim results

---
+++ R_Programming/R_Programming/Loops/interim_results.R
@@ -0,0 +1,16 @@
+num <- c(1, 4, 1, 2, 1, 10)
+prefix_sum <- numeric(length(num))
+
+for (i in 1: length(num)) {
+  prefix_sum[i] <- sum(num[1: i])
+}
+prefix_sum
+
+
+prefix_sum <- numeric(0)
+for (i in 1: length(num)) {
+  prefix_sum <- c(prefix_sum, sum(num[1: i]))
+}
+prefix_sum
+mat <- matrix(1:5, nrow = 1)
+mat
