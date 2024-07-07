a31b26659a939d8e305d77af0f9aa772d55b85bd
Shoyeb45 <shoyebff45@gmail.com>
2024-07-07 17:55:08 +0530

    Combine lists

---
+++ R_Programming/R_Programming/Lists/combineFunctions.R
@@ -0,0 +1,11 @@
+list1 <- list(3, 4)
+list2 <- list(100, 200)
+# combined by c - combines elements
+res <- c(list1, list2)
+res
+length(res)
+
+#Combined by list
+res <- list(list1, list2)
+res
+length(res)
