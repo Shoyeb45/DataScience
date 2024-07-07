e091d0591780c7585cdabfe95b5b6c15c1707e73
Shoyeb45 <shoyebff45@gmail.com>
2024-07-07 17:52:52 +0530

    Recursive structure and attributes of lists

---
+++ R_Programming/R_Programming/Lists/recursiveStructure.R
@@ -0,0 +1,12 @@
+person <- list(
+  Names = c(name = "Shoyeb", age = "19"), date_of_birth = list("June 17")
+)
+person
+person$date_of_birth
+
+
+# attributes of list
+class(person)
+names(person)
+str(person)
+typeof(person)
