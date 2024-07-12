8428548840faf36718f14d54587882812a791edc
Shoyeb45 <shoyebff45@gmail.com>
2024-07-12 21:55:04 +0530

    The Subset Function

---
+++ R_Programming/R_Programming/Dataframe/subset_function.r
@@ -0,0 +1,17 @@
+df <- data.frame(
+  name = c("Petra", "Jochen", "Alexander"), # character
+  age = c(35L, 21L, 12L), # integer
+  height = c(1.72, 1.65, 1.39), # numeric
+  austrian = c(FALSE, TRUE, TRUE), # logical
+  stringsAsFactors = FALSE # default
+)
+
+subset(df, age > 18, select = c(height, austrian))
+subset(df, !austrian)
+x <- subset(df, age > 18, austrian)
+y <- subset(df, age > 18, austrian, drop = TRUE)
+x
+y
+class(x)
+class(y)
+
