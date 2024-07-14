4c2ccf6cb26d6cea88f37dedd8bf910f08d6a898
Shoyeb45 <shoyebff45@gmail.com>
2024-07-14 16:56:28 +0530

    Subsetting Dataframe

--- R_Programming/R_Programming/Dataframe/subsetting_dataframe.r
+++ R_Programming/R_Programming/Dataframe/subsetting_dataframe.r
@@ -12,11 +12,17 @@ df["name"]
 df$is_married
 df[c("is_married", "name")]
 df[c(TRUE, FALSE, TRUE, TRUE, TRUE)]
-
+df1 <- data.frame(
+  name = c("Ram", "Rahul", "ravi"),
+  age = 15:17,
+  height = c(1.62, 1.59, 1.68)
+)
+df1[[c(3, 2)]]
 
 # Matrix style subsetting
-df[4,] 
+df[4, ]
 df[c(1, 2), ]
 df[, 4]
 df[1, 1:3]
 df[1:3, 2]
+df[[2]][2]
