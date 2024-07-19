b1abe67e320d9257e1d58ccb00596e1671120004
Shoyeb45 <shoyebff45@gmail.com>
2024-07-19 09:36:47 +0530

    clauses

---
+++ SQL/clause.sql
@@ -0,0 +1,14 @@
+use classicmodels;
+show tables;
+
+select * from offices;
+
+select *
+from offices
+where country like "%A";
+
+select * from orders;
+
+select *
+from orders
+order by customerNumber	;
